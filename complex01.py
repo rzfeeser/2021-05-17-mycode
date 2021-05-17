b63404ee8537eaad9540768de1f1764f797ffb83
rzfeeser <rzfeeser@users.noreply.github.com>
2021-05-17 20:14:09 +0000

    About complex lists

---
+++ complex01.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# create a list called list1
+list1 = ["cisco_nxos", "arista_eos", "cisco_ios"]
+# display list1
+print(list1)
+
+print(list1[2], end=" ") # end with a WHITESPACE not a newline
+print(list1[1], end=" ") # end with a WHITESPACE not a newline
+print(list1[0])
