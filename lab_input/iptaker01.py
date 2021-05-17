43d923c7304695a0661b54e94c96b17edaf8c6cf
rzfeeser <rzfeeser@users.noreply.github.com>
2021-05-17 17:00:39 +0000

    Going to lunch on day 1

---
+++ lab_input/iptaker01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# the input function always produces a string
+user_input = input("Please enter an IPv4 IP address: ")
+
+# this is called concatination (addition)
+print("You told me the IPv4 address is:" + user_input)
+
+# this is just asking for two objects to be printed...
+print("You told me the IPv4 address is:", user_input)
+
+user_input = input("what is the vendor for this device?")
+
+print("The vendor for this device is", user_input)
