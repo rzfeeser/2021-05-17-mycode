6165ca2b6c44c6da9528e51897db380477e8bc32
rzfeeser <rzfeeser@users.noreply.github.com>
2021-05-21 14:03:10 +0000

    review friday morning

---
+++ apireview.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Alta3 Research | RZFeeser@alta3.com
+   API Review Friday Morning."""
+
+API = "https://api.nasa.gov/planetary/apod?api_key="
+
+# python3 -m pip install requests
+import requests
+
+def getkey():
+    """Pull the NASA API Key"""
+    with open("/home/student/.ssh/nasa_key") as keyfile:
+        apikey = keyfile.read().strip()
+    return apikey
+
+def main():
+    """Called at runtime"""
+    r = requests.get(f"{API}{getkey()}")
+
+    # figuring out techniques for manipulating our returned data
+    #print(r.json().get("hdurl").split("/")[-1])
+
+    imgurl = r.json().get("hdurl")
+    imgdata = requests.get(imgurl).content
+    with open(f"/home/student/static/{imgurl.split('/')[-1]}", 'wb') as handler:
+        handler.write(imgdata)
+
+if __name__ == "__main__":
+    main()
+
